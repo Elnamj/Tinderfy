3334a48678f62b61607596d164a6a9878890cbe4
Elna Mattsson Johansson <elnamattssonjohansson@n176-p173.eduroam.kth.se>
2019-04-12 16:07:54 +0200

    app clean

--- src/App.js
+++ src/App.js
@@ -13,43 +13,16 @@ class App extends Component {
   constructor() {
     super();
 
-    /*if (token) {
-      spotifyApi.setAccessToken(token);
-    }
-    */
     this.state = {
       title: "Tinderfy",
-
     }
   }
-/*
-  search(q) {
-      const url = "https://api.spotify.com/v1/search?q=Beyonce&type=track";
-      return fetch(url, this.state.httpOptions).then(response => response.json())
-      .catch(this.handleError);
-  }
-*/
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
-          {/*<a href='http://localhost:8888' >
-            <button>Login to Spotify </button>
-          </a>*/}
-          <div>
 
-          </div>
-
-          {/* this.state.loggedIn &&
-          <button onClick={() => this.search()}>
-            Check genre
-          </button>*/
-          }
-
-
-          {/* We rended diffrent component based on the path */}
-          {/* <Route exact path="/" component={Welcome} /> */}
           <Route
             path="/filter"
             render={() => <Filter model={model} />}
