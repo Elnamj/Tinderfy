335cc8918590f6f25f6aee1c4d84d23bc4d41f1e
Elna Mattsson Johansson <elnamattssonjohansson@n155-p151.eduroam.kth.se>
2019-04-16 12:55:38 +0200

    url heroku 2

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -17,7 +17,7 @@ class Welcome extends Component {
             {/* <a href="https://tinderfy-backend.herokuapp.com/login" > */}
             {/* <a href="http://localhost:8888/login">*/}
           <div className="col-6">
-             <a href="http://localhost:8888/login" >
+             <a href="https://tinderfy-backend.herokuapp.com/login" >
              <button className="cool-btn btn1 btn" id="btn2">Start</button>
              </a>
           </div>
