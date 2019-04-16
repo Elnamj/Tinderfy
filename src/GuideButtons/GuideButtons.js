e3de46feab7c117d6f8aa59358a2eaa0a500397d
Elna Mattsson Johansson <elnamattssonjohansson@n155-p151.eduroam.kth.se>
2019-04-16 12:51:31 +0200

    url heroku

--- src/GuideButtons/GuideButtons.js
+++ src/GuideButtons/GuideButtons.js
@@ -17,7 +17,7 @@ class GuideButtons extends Component {
             {/* <a href="https://tinderfy-backend.herokuapp.com/login" > */}
             {/*<a href="http://localhost:8888/login">*/}
           <div className="col-6">
-             <a href="http://localhost:8888/login">
+             <a href="https://tinderfy-backend.herokuapp.com/login" >
              <button className="btn btn1 cool-btn" id="btn1">Start</button>
              </a>
           </div>
